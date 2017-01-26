7bc7695f6f46daff2450f8e4d908b6397013914c
digitalyuki <digitalyuki@users.noreply.github.com>
2017-01-26 10:58:08 -0500

    Make more things available as env vars and add wording regarding instance-id (#3)
    * Change default to *not* list all nodes
    
    * Add more environment variable option to be-ethos
    
    * Clean up env variables help and add BE_ETHOS_AWS_SOURCE_ROOT env var
    
    * Expand help to list instance-id as well
    
    * fix BE_ETHOS_AWS_SOURCE_ROOT bug

--- plugins/available/be-ethos.plugin.bash
+++ plugins/available/be-ethos.plugin.bash
@@ -4,30 +4,38 @@ about-plugin 'behancecompute ethos ssh and node listing helper'
 function __be-ethos-usage {
     echo -e "Usage: be-ethos <tier> <command>"
     echo -e "  <tier>: dev | stage | prod"
-    echo -e "  <command>: ssh <ip|hosttype> | ls [<search_string>] | scpkey [<ip>]"
-    echo -e "    ssh <ip|hosttype>: ssh to specific ip or first node of the hosttype ('control'/'worker'/'proxy'/'bastion')"
+    echo -e "  <command>: ssh <ip|hosttype|instance-id> | ls [<search_string>] | scpkey [<ip>]"
+    echo -e "    ssh <ip|hosttype|instance-id>: ssh to specific ip, first node of the hosttype ('control'/'worker'/'proxy'/'bastion'), or instance-id"
     echo -e "    ls [<search_string>]: list instances with optional search string"
-    echo -e "    scpkey [<ip>]: copy required key to bastion host, optionally specify internal ip for specific bastion host if there are several"
+    echo -e "    scpkey [<ip>]: copy required key to bastion host, optionally specify internal or external ip for specific bastion host if there are several"
     echo -e "\nDescription: Does nice things with ec2 and behancecompute, uses jungle, ~/.aws/<tier>.sh, cloudops-be-app-<tier>.pem keys and virtualenv 'be-ethos'"
     echo -e "\nExample: be-ethos dev ssh control"
-    echo -e "\nAvailable environnment variables:"
-    echo -e "  BE_ETHOS_PRODUCT_NAME: default is 'behanceco'"
-    echo -e "  BE_ETHOS_VIRTUALENV_NAME: set to your preferred virtualenv name, or \"NO\" to not use virtualenv, if not specified it will use the 'be-ethos' virtualenv"
-    echo -e "  BE_ETHOS_AUTO_LS: set to \"NO\" to turn off automatically ls'ing nodes after commands"
+    echo -e "\nAvailable environnment variables <NAME> ('<current_value>'): <Description>"
+    echo -e "  BE_ETHOS_PRODUCT_NAME ('$BE_ETHOS_PRODUCT_NAME'): default is 'behanceco'"
+    echo -e "  BE_ETHOS_VIRTUALENV_NAME ('$BE_ETHOS_VIRTUALENV_NAME'): set to your preferred virtualenv name, or \"NO\" to not use virtualenv, if not specified it will use the 'be-ethos' virtualenv"
+    echo -e "  BE_ETHOS_AUTO_LS ('$BE_ETHOS_AUTO_LS'): set to \"YES\" to turn on automatically ls'ing nodes after commands"
+    echo -e "  BE_ETHOS_LOCALUSER_KEY_PATH_PREFIX ('$BE_ETHOS_LOCALUSER_KEY_PATH_PREFIX'): path and prefix to dev/stage/prod cloudops keys (minus tier), default is '${HOME}/.ssh/cloudops-be-app-'"
+    echo -e "  BE_ETHOS_AWS_SOURCE_ROOT ('$BE_ETHOS_AWS_SOURCE_ROOT'): directory path to auto-source your aws credentials, at <BE_AWS_SOURCE_ROOT>/<tier>.sh, set to \"NO\" to turn off sourcing, default is '${HOME}/.aws'"
 }
 
 function be-ethos {
+  HOST_IP=""
+  REMOTEUSER_NAME="core"
+  # LOAD defaults if BE_ETHOS_* env variables are not set
   if [[ "$BE_ETHOS_PRODUCT_NAME" == "" ]] ; then
     BE_ETHOS_PRODUCT_NAME="behanceco"
   fi
-  HOST_IP=""
-  LOCALUSER_KEY_PATH_PREFIX="${HOME}/.ssh/cloudops-beh-app-" # path and prefix to dev/stage/prod cloudops keys 
-  REMOTEUSER_NAME="core"
+  if [[ "$BE_ETHOS_LOCALUSER_KEY_PATH_PREFIX" == "" ]] ; then
+    BE_ETHOS_LOCALUSER_KEY_PATH_PREFIX="${HOME}/.ssh/cloudops-beh-app-" # path and prefix to dev/stage/prod cloudops keys
+  fi
   # set BE_ETHOS_VIRTUALENV_NAME env variable to "NO" to turn off, or set to your own preferred virtualenv
   if [[ "$BE_ETHOS_VIRTUALENV_NAME" != "NO" && "$BE_ETHOS_VIRTUALENV_NAME" == "" ]]; then
     BE_ETHOS_VIRTUALENV_NAME="be-ethos" # set to "NO" to turn off
   fi
-  AWS_SOURCE_ROOT="${HOME}/.aws" #set to "" to turn off aws source scripting for credentials
+  # set BE_ETHOS_AWS_SOURCE_ROOT to "NO" to turn off, or set your own aws source scripting directory
+  if [[ "$BE_ETHOS_AWS_SOURCE_ROOT" != "NO" && "$BE_ETHOS_AWS_SOURCE_ROOT" == "" ]] ; then
+    BE_ETHOS_AWS_SOURCE_ROOT="${HOME}/.aws"
+  fi
   if [[ "$1" == "" || "$2" == "" ]]; then
     if [[ "$1" != "" ]] ; then
       echo "Missing required parameters"
@@ -41,11 +49,11 @@ function be-ethos {
     __be-ethos-usage
     return 1
   fi
-  LOCALUSER_KEY_PATH="${LOCALUSER_KEY_PATH_PREFIX}${ENVIRON}.pem"
+  LOCALUSER_KEY_PATH="${BE_ETHOS_LOCALUSER_KEY_PATH_PREFIX}${ENVIRON}.pem"
   REMOTEUSER_KEY_NAME="cloudops-beh-app-${ENVIRON}.pem"
   REMOTEUSER_KEY_PATH="/home/${REMOTEUSER_NAME}/.ssh/${REMOTEUSER_KEY_NAME}"
-  if [[ "$AWS_SOURCE_ROOT" != "" ]] ; then 
-    source ${AWS_SOURCE_ROOT}/${ENVIRON}.sh
+  if [[ "$BE_ETHOS_AWS_SOURCE_ROOT" != "NO" ]] ; then
+    source ${BE_ETHOS_AWS_SOURCE_ROOT}/${ENVIRON}.sh
   fi
   if [[ "$BE_ETHOS_VIRTUALENV_NAME" != "NO" ]] ; then
     workon $BE_ETHOS_VIRTUALENV_NAME
@@ -72,7 +80,7 @@ function be-ethos {
 
   if [[ "$2" == "ssh" ]]; then
 
-    if [[ "$BE_ETHOS_AUTO_LS" != "NO" ]] ; then 
+    if [[ "$BE_ETHOS_AUTO_LS" == "YES" ]] ; then 
       echo -e "$JUNGLE_MAIN_QUERY\n"
     fi
     if [[ $3 =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
@@ -133,9 +141,15 @@ EOF
 
     if [[ "$3" != "" ]] ; then
       HOST_JUNGLE_BASTION="$(echo "$JUNGLE_MAIN_QUERY" | grep "$3" | grep 'bastion')"
-      HOST_IP_BASTION="$(echo "$HOST_JUNGLE_BASTION" | awk '{print $5}')"
-      HOST_NAME_BASTION="$(echo "$HOST_JUNGLE_BASTION" | awk '{print $1}')"
+      if [[ "$HOST_JUNGLE_BASTION" != "" ]] ; then
+        HOST_IP_BASTION="$(echo "$HOST_JUNGLE_BASTION" | awk '{print $5}')"
+        HOST_NAME_BASTION="$(echo "$HOST_JUNGLE_BASTION" | awk '{print $1}')"
+      else
+        echo "Bastion host not found"
+        return 1
+      fi
     fi
+    echo -e "Copying cloudops key to bastion host $HOST_NAME_BASTION at $HOST_IP_BASTION...\n"
     scp -i ${LOCALUSER_KEY_PATH} ${LOCALUSER_KEY_PATH} ${REMOTEUSER_NAME}@${HOST_IP_BASTION}:${REMOTEUSER_KEY_PATH}
 
   else
